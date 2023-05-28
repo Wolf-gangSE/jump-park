f1cbbe5a2cc5c52531b25492f09ad810d0f4bdb3
Lucas Henrique <lucasmonte2011@gmail.com>
2023-05-28 02:17:32 -0300

    feat: add getServiceOrders to ApiController

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -20,11 +20,11 @@ class ApiController extends Controller
                 'exitDateTime' => 'required|date',
                 'priceType' => 'required|string',
                 'price' => 'required|numeric',
-                'user_id' => 'required|int',
+                'userId' => 'required|int',
             ]); 
 
             # Verify if user exists
-            $user = User::where('id', $request->user_id)->first();
+            $user = User::where('id', $request->userId)->first();
             if (!$user) {
                 return response()->json(['message' => 'User not found!'], 404);
             }
@@ -36,13 +36,52 @@ class ApiController extends Controller
                 'exitDateTime' => $request->exitDateTime,
                 'priceType' => $request->priceType,
                 'price' => $request->price,
-                'userId' => $user->id,
+                'userId' => $request->userId,
             ]);
+
+            # Associate user to service order
+            $serviceOrder->user()->associate($user);
+
+            # Save service order
+            $serviceOrder->save();
 
             return response()->json(['message' => 'Service order created successfully!'], 200);
 
         } catch (\Exception $e) {
             return response()->json(['message' => "Error creating service order: {$e->getMessage()}"], 500);
+        }
+    }
+
+    public function getServiceOrders(Request $request)
+    {
+        try {
+
+            # Validate request
+            $request->validate([
+                'vehiclePlate' => 'nullable|string|max:7',
+            ]);
+
+            # Get service orders paginated
+            $results = ServiceOrder::where('vehiclePlate', 'like', "%{$request->vehiclePlate}%")
+                ->paginate(5);
+
+            $serviceOrders = $results->items();
+
+            # Add user name to service orders and remove user_id
+            foreach ($serviceOrders as $serviceOrder) {
+                $serviceOrder->userName = $serviceOrder->user->name;
+                unset($serviceOrder->user_id);
+                unset($serviceOrder->user);
+            }
+
+            # Return service orders
+            return response()->json([
+                'message' => 'Service orders retrieved successfully!', 
+                'serviceOrders' => $serviceOrders
+            ], 200);
+
+        } catch (\Exception $e) {
+            return response()->json(['message' => "Error getting service orders: {$e->getMessage()}"], 500);
         }
     }
 }
